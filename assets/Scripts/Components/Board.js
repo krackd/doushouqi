d4da5a671c651a567c3d4ff7ad5f6a13bcb6d462
krackd <43828895+krackd@users.noreply.github.com>
2020-03-29 03:24:41 +0200

    Managing collisions

--- assets/Scripts/Components/Board.js
+++ assets/Scripts/Components/Board.js
@@ -48,24 +48,20 @@ cc.Class({
 
         var isGroundCollision = this.map.isGroundCollision(event.getLocationInView());
         if (isGroundCollision) {
-            cc.log('Clicked on a collision tile');
             return;
         }
 
         var isWaterCollision = this.map.isWaterCollision(event.getLocationInView());
         if (isWaterCollision) {
-            cc.log('Clicked on a water tile');
             return;
         }
 
         var selectedPos = this.map.getPawnPosition(this.selected);
         var tilePos = this.map.getTilePosition(event.getLocationInView());
         if (JSON.stringify(selectedPos) === JSON.stringify(tilePos)) {
-            cc.log('Clicked on selected pawn');
             return;
         }
 
-        cc.log('Moving selected pawn');
         var target = this.map.getPositionFromTilePosition(tilePos);
         var distance = tilePos.sub(selectedPos).magSqr();
         
